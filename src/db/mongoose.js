1b834e0f53057303f3db75340fdf286474bea619
Aditya Saxena <adityasmksaxena@gmail.com>
2018-04-30 02:05:31 +0530

    mongodb remote url change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,15 +3,9 @@ import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 
 import dotenv from 'dotenv';
-
 const res = dotenv.config();
 if (res.error) throw res.error
 
-let db = {
-  localhost: 'mongodb://localhost:27018/TodoApp',
-  mlab: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds263619.mlab.com:63619/todo-app`
-};
-
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost );
+mongoose.connect( process.env.MONGODB_URI );
 
 export default mongoose;
