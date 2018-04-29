71529954e78d2feea869703915695578b8c6265a
Aditya Saxena <adityasmksaxena@gmail.com>
2018-04-30 02:09:14 +0530

    mongodb remote url change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
-import dotenv from 'dotenv';
-const res = dotenv.config();
-if (res.error) throw res.error
-
 mongoose.connect( process.env.MONGODB_URI );
 
 export default mongoose;
