c985578e3d9cd80f17d93d7e9d996a42b5851a68
Aditya Saxena <adityasmksaxena@gmail.com>
2018-04-29 09:15:29 +0530

    Added GET /todos/:id

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from './db/mongoose';
+import {ObjectId} from 'mongodb';
 
 import bodyParser from 'body-parser';
 
@@ -34,6 +35,18 @@ app.get('/todos', (req, res) => {
   }).catch((err) => {
     res.status(400).send(err);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if(!ObjectId.isValid(id)) return res.status(404).send();
+  Todo.findById(id)
+  .then((todo) => {
+    if(!todo) return res.status(404).send();
+    res.send({todo});
+  }).catch((err) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(3001, () => {
