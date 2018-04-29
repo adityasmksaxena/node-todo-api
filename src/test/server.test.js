4f2fe85aa43e73841c22e956e84e99b6c8a6c4b2
Aditya Saxena <adityasmksaxena@gmail.com>
2018-04-29 12:01:30 +0530

    Added text cases for GET /todos/:id

--- src/test/server.test.js
+++ src/test/server.test.js
@@ -1,4 +1,5 @@
 import expect from 'expect';
+import {ObjectID} from 'mongodb';
 import request from 'supertest';
 
 import app from '../server';
@@ -7,10 +8,13 @@ import Todo from '../models/todo';
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: "Todo 1"
   }, {
+    _id: new ObjectID(),
     text: "Todo 2"
   }, {
+    _id: new ObjectID(),
     text: "Todo 3"
   }
 ];
@@ -74,4 +78,33 @@ describe('GET /todos', () => {
     .end(done);
   });
 
+});
+
+describe('GET /todos/:id', () => {
+  
+  it('should return todo doc', (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+  
+  it('should return 404 if todo not found', (done) => {
+    let hexId = new ObjectID().toHexString();
+    request(app)
+    .get(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
+  });
+  
+  it('should return 404 for invalid id', (done) => {
+    request(app)
+    .get('/todos/123abc')
+    .expect(404)
+    .end(done);
+  });
+  
 });
