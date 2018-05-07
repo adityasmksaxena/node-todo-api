41518607b6e044072489885acdc924915e4cd217
Aditya Saxena <adityasmksaxena@gmail.com>
2018-05-07 12:58:20 +0530

    testing

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,13 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config.json');
+// const config = require('./config.json');
 
 if (env === 'development' || env === 'test') {
-  const envConfig = config[env];
-  Object.keys(envConfig).forEach((key) => {
-    process.env[key] = envConfig[key];
-  });
+  // const envConfig = config[env];
+  // console.log(envConfig);
+  // Object.keys(envConfig).forEach((key) => {
+  //   process.env[key] = envConfig[key];
+  // });
+  process.env.PORT = '3001';
+  process.env.MONGODB_URI = 'mongodb://localhost:27018/TodoApp';
+  process.env.JWT_SECRET = 'alksDETpAkjfskOPI';
 }
